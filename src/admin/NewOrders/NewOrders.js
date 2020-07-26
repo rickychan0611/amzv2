f86dcce454380f2e2a79764a647ebe989136423d
rickychan0611 <ric0611@gmail.com>
2020-07-25 23:52:20 -0700

    add new order report

--- src/admin/NewOrders/NewOrders.js
+++ src/admin/NewOrders/NewOrders.js
@@ -1,15 +1,36 @@
-import React from 'react'
-import { Container } from 'semantic-ui-react'
-import './styles.css'
+import React from "react";
+import { Container } from "semantic-ui-react";
+import "./styles.css";
+import ClipboardJS from "clipboard";
 
 const NewOrders = () => {
+  new ClipboardJS(".btn");
+
   return (
     <div className="new-orders-container">
-            <h2>产品登记</h2>
+      <h2>New order report</h2>
+      <h3>seller name</h3>
+      <div id="index">
+        <div>NEW ORDERS:</div>
+        <div>product name: 701-12345678-12345678</div>
+        <div>product name: 701-12345678-12345678</div>
+        <div>product name: 701-12345678-12345678</div>
+        <br />
+        <div>NEW REVIEW:</div>
+        <div>product name : 701-12345678-12345678 : review url</div>
+        <div>product name : 701-12345678-12345678 : review url</div>
+        <div>product name : 701-12345678-12345678 : review url</div>
+      </div>
+      <br />
+      <button className="btn" data-clipboard-target="#index">
+        Copy
+      </button> &nbsp;&nbsp;
 
-      
+      <button>Done</button>
+      <br />
+      <hr />
     </div>
-  )
-}
+  );
+};
 
-export default NewOrders
+export default NewOrders;
--- src/context/ProductsContext.js
+++ src/context/ProductsContext.js
@@ -1,41 +1,45 @@
 import React, { createContext, useState, useEffect } from "react";
 import firebase from "firebase";
 import db from "../firestore";
-import { useHistory } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export const ProductsContext = createContext();
 
 const ProductsContextProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
+  let location = useLocation();
 
   useEffect(() => {
-    setLoading(true);
-    let tempArr = [];
-    db.collection("products")
-      .where("active", "==", true)
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          tempArr.push(doc.data());
-        });
-        //sort
-        async function sortArr() {
-          await tempArr.sort((current, prev) => {
-            return new Date(prev.createAt) - new Date(current.createAt);
+    let isAdmin = location.pathname.includes("myadmin");
+    if (!isAdmin) {
+      setLoading(true);
+      let tempArr = [];
+      db.collection("products")
+        .where("active", "==", true)
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            tempArr.push(doc.data());
           });
-          setProducts(tempArr);
-          setLoading(false);
-        }
-        sortArr();
-      });
+          //sort
+          async function sortArr() {
+            await tempArr.sort((current, prev) => {
+              return new Date(prev.createAt) - new Date(current.createAt);
+            });
+            setProducts(tempArr);
+            setLoading(false);
+          }
+          sortArr();
+        });
+    }
   }, []);
 
   return (
     <ProductsContext.Provider
       value={{
         products,
-        loading
+        loading,
       }}
     >
       {children}
